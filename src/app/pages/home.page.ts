a324e83cc8a0d588d0812fe6fd0bfee1b1e97698
itelemarked <itelemarked@icloud.com>
2022-10-07 00:09:57 +0200

    add home page

--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -2,24 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  styles: [],
   template: `
     <ion-header>
       <ion-toolbar>
         <ion-title>Home</ion-title>
+        <ion-buttons slot="end">
+          <ion-button slot="icon-only" routerLink="auth">
+            <ion-icon name="person-circle-outline"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
     <ion-content>
-        <ion-button fill="outline" (click)="onClick()">btn</ion-button>
-        <ion-fab-button color="primary">Primary</ion-fab-button>
+      <ion-button>go to training</ion-button>
     </ion-content>
   `,
-  styles: [],
 })
-export class HomePage implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  onClick() {}
-}
+export class HomePage {}

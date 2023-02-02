1d02d93fca541752dcdab9162d7dc5ac0a53d30b
greg <greg@greg.com>
2023-02-02 17:56:06 +0100

    commit

--- src/app/testing/test2/utils/multiple/interfaces.ts
+++ src/app/testing/test2/utils/multiple/interfaces.ts
@@ -1,5 +1,24 @@
 
-export type TId = `${number}x${number}`
+export type TId = `${number}x${number}`;
+
+
+export type PositiveOrZeroNumber = number & {_type: 'PositiveOrZeroNumber' };
+
+export function isPositiveOrZeroNumber(n: number): n is PositiveOrZeroNumber {
+  return n >= 0;
+}
+
+
+export type PositiveOrZeroNumberArray = number[] & { _type: 'PositiveOrZeroNumberArray'}
+
+export function isPositiveOrZeroNumberArray(arr: number[]): arr is PositiveOrZeroNumberArray {
+  const invalidFound = arr.find(a => a < 0);
+  return invalidFound === undefined;
+}
+
+
+export type UniqueNumberArray = number[] & { _type: 'UniqueNumberArray'}
+
 
 export interface Multiple {
   readonly id: TId,
@@ -7,9 +26,11 @@ export interface Multiple {
   fails: number
 }
 
+
 export interface Training {
   successesRequired: number,
   multiples: Multiple[]
 }
+
 
 
--- src/app/testing/test2/utils/multiple/multiple.ts
+++ src/app/testing/test2/utils/multiple/multiple.ts
@@ -1,4 +1,4 @@
-import { Multiple, TId } from "./interfaces";
+import { Multiple, PositiveOrZeroNumber, PositiveOrZeroNumberArray, TId } from "./interfaces";
 
 
 function _getMinAndMaxFromId(id: TId): {min: number, max: number} {
@@ -15,10 +15,12 @@ function _getMinAndMaxFromId(id: TId): {min: number, max: number} {
   return n1 < n2 ? {min: n1, max: n2} : {min: n2, max: n1};
 }
 
+
 export function getResult(id: TId) {
   const {min, max} = _getMinAndMaxFromId(id);
   return min * max;
 }
+
 
 export function getRandomFilteredMultiple(multiples: Multiple[], filterFn: (m: Multiple) => boolean): Multiple | null {
   const filtered = multiples.filter(filterFn);
@@ -26,3 +28,50 @@ export function getRandomFilteredMultiple(multiples: Multiple[], filterFn: (m: M
 
   return filtered[randomIdx];
 }
+
+
+
+export const containsNegativeNumber = (arr: number[]): boolean => {
+  const foundNegative = arr.find(a => a < 0);
+  return foundNegative !== undefined;
+}
+
+export const containsDuplicateNumber = (arr: number[]): boolean => {
+  const foundDuplicate = arr.find((item, idx, self) => self.indexOf(item) !== idx);
+  return foundDuplicate !== undefined;
+}
+
+
+export function generateMultiplesByBases(bases: number[], factors: number[]): Multiple[] {
+
+  if (containsNegativeNumber(bases) || containsDuplicateNumber(bases)) {
+    throw new Error('Bases contains negative or duplicate number...')
+  }
+
+  if (containsNegativeNumber(factors) || containsDuplicateNumber(factors)) {
+    throw new Error('Factors contains negative or duplicate number...')
+  }
+
+  const sortedBases = bases.sort((a,b) => a-b);
+  const sortedFactors = factors.sort((a,b) => a-b);
+
+  const results: Multiple[] = [];
+  for (let i = 0; i < bases.length; i++) {
+    const baseItem = sortedBases[i];
+    for (let j = 0; j < factors.length; j++) {
+      const factorItem = sortedFactors[j];
+
+      if (!bases.includes(factorItem) || factorItem >= baseItem) {
+        results.push({
+          id: `${baseItem}x${factorItem}`,
+          successes: 0,
+          fails: 0
+        })
+      }
+    }
+  }
+
+  return results;
+}
+
+
